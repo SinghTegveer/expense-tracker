6d11d4b12a3480c0a1eadf6af0d8154297d8eaa3
NishchalPersonal <bhardwajnishchal1999@gmail.com>
2023-06-20 18:05:34 -0400

    first comment

--- expensetrackerapi/src/main/java/com/eta/expensetrackerapi/ExpensetrackerapiApplication.java
+++ expensetrackerapi/src/main/java/com/eta/expensetrackerapi/ExpensetrackerapiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExpensetrackerapiApplication {
-
+	// main runner class
 	public static void main(String[] args) {
 		SpringApplication.run(ExpensetrackerapiApplication.class, args);
 	}
